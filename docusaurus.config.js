5655cff205ed67bdf7077e07183b69a5070f7d56
beeena <mubina.ghazi@gmail.com>
2022-01-20 19:02:59 +0500

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://beeena.github.io',
+  baseUrl: '/test-ghp/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
+  // url: 'https://https://beeena.github.io/', // Your website URL
+  projectName: 'test-ghp',
+  organizationName: 'beeena',
 
   presets: [
     [
