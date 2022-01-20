85ac33dba29eed136472a8bb87b90d5ee89816a9
beeena <mubina.ghazi@gmail.com>
2022-01-20 19:36:46 +0500

    changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://beeena.github.io/test-ghp/',
-  baseUrl: '/test-ghp/index.html/',
+  url: 'https://beeena.github.io/',
+  baseUrl: '/test-ghp/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
