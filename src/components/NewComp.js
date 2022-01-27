f64d8fa8e968b3c5c9dc8420f8200a5fa93951b3
beeena <mubina.ghazi@gmail.com>
2022-01-27 13:02:00 +0500

    changes

---
+++ src/components/NewComp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className='container'>
+        <div className='row'>hellooo</div>
+      </div>
+    </section>
+  );
+}
